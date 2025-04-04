9d73fb7ab0383d09d69a8fc066865f0f1dac57e4
cano0102 <canoanderson418@gmail.com>
2025-04-04 00:02:24 -0500

    agredado dia 3 y 4

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 console.log("ESTE ES MI DIA A DIA EN PROGRAMACION")
 console.log("estuve practicando git")
+console.log("ejercicios de programacion")
+console.log("ejercicios de programacion y configure mi github")
