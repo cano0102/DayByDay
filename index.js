5db056db156a8925d0d49f687cd0f13ebf8c6dce
cano0102 <canoanderson418@gmail.com>
2025-04-07 22:13:33 -0500

    dia lunes 07 04 2

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ console.log("estuve practicando git")
 console.log("ejercicios de programacion")
 console.log("ejercicios de programacion y configure mi github")
 console.log("hice ejercicios de pyton ")
-console.log("hice un examen en el sena de pyton")
+console.log("hice un examen en el sena de pyton , ejercicio de javascript")
+
+
