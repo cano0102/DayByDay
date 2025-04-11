84c7825ac8fe2238744808e708e85c59a59f993b
cano0102 <canoanderson418@gmail.com>
2025-04-10 22:32:13 -0500

    10/04/2025

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ console.log("ejercicios de programacion y configure mi github")
 console.log("hice ejercicios de pyton ")
 console.log("hice un examen en el sena de pyton , ejercicio de javascript")
 console.log("Agregue la segunda funcionalidad del proyecto : GESTOR DE PACIENTES ")
-console.log("Agre")
+console.log("hice algunas cosas del protecto: GESTOR DE PACIENTES")
+console.log("aprendiendo un poco de git")
 
 
 
